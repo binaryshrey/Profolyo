7024779da024fabb82976510e288951d06eba7f4
binaryshrey <shreyansh.saurabh2016@vitstudent.ac.in>
2024-08-20 09:31:42 +0530

    add integrations data

---
+++ src/services/data/integrations.js
@@ -0,0 +1,156 @@
+export const integrations = [
+    {
+        'name': 'Behance',
+        'url': 'https://www.behance.net/',
+        'img': 'https://raw.githubusercontent.com/binaryshrey/Profolyo/feat/onboarding/src/assets/integrations/behance.svg',
+        'description': 'Behance is an online platform to showcase & discover creative work.',
+        'type': 'Design'
+    },
+    {
+        'name': 'Discord',
+        'url': 'https://discord.com/',
+        'img': 'https://raw.githubusercontent.com/binaryshrey/Profolyo/feat/onboarding/src/assets/integrations/discord.svg',
+        'description': 'Discord is a voice, video and text communication service to talk and hang out with your friends and communities.',
+        'type': 'Social'
+    },
+    {
+        'name': 'Dribbble',
+        'url': 'https://dribbble.com/',
+        'img': 'https://raw.githubusercontent.com/binaryshrey/Profolyo/feat/onboarding/src/assets/integrations/dribbble.svg',
+        'description': 'Dribbble is a community of designers sharing screenshots of their work, process, and projects.',
+        'type': 'Design'
+    },
+    {
+        'name': 'Dropbox',
+        'url': 'https://www.dropbox.com/',
+        'img': 'https://raw.githubusercontent.com/binaryshrey/Profolyo/feat/onboarding/src/assets/integrations/dropbox.svg',
+        'description': 'Dropbox is a file hosting service that offers cloud storage, file synchronization, personal cloud, and client software.',
+        'type': 'Other'
+    },
+    {
+        'name': 'Facebook',
+        'url': 'https://www.facebook.com/',
+        'img': 'https://raw.githubusercontent.com/binaryshrey/Profolyo/feat/onboarding/src/assets/integrations/facebook.svg',
+        'description': 'Facebook is a social networking service that allows users to connect with friends, family and colleagues.',
+        'type': 'Social'
+    },
+     {
+        'name': 'Figma',
+        'url': 'https://www.figma.com/',
+        'img': 'https://raw.githubusercontent.com/binaryshrey/Profolyo/feat/onboarding/src/assets/integrations/figma.svg',
+        'description': 'Figma is a cloud-based design tool that is similar to Sketch in functionality and features, but with big differences that make Figma better for team collaboration.',
+        'type': 'Design'
+    },
+    {
+        'name': 'Github',
+        'url': 'https://www.github.com/',
+        'img': 'https://raw.githubusercontent.com/binaryshrey/Profolyo/feat/onboarding/src/assets/integrations/github.svg',
+        'description': 'GitHub is a web-based version-control and collaboration platform for software developers.',
+        'type': 'Developer'
+    },
+    {
+        'name': 'Gitlab',
+        'url': 'https://about.gitlab.com/',
+        'img': 'https://raw.githubusercontent.com/binaryshrey/Profolyo/feat/onboarding/src/assets/integrations/gitlab.svg',
+        'description': 'GitLab is a web-based DevOps lifecycle tool that provides a Git repository manager providing wiki, issue-tracking and CI/CD pipeline features, using an open-source license.',
+        'type': 'Developer'
+    },
+    {
+        'name': 'Google',
+        'url': 'https://www.google.com/',
+        'img': 'https://raw.githubusercontent.com/binaryshrey/Profolyo/feat/onboarding/src/assets/integrations/google.svg',
+        'description': 'Google is a multinational corporation that specializes in Internet-related services and products.',
+        'type': 'Other'
+    },
+    {
+        'name': 'Google',
+        'url': 'https://www.google.com/',
+        'img': 'https://raw.githubusercontent.com/binaryshrey/Profolyo/feat/onboarding/src/assets/integrations/google.svg',
+        'description': 'Google is a multinational corporation that specializes in Internet-related services and products.',
+        'type': 'Other'
+    },
+    {
+        'name': 'LinkedIn',
+        'url': 'https://www.linkedin.com/',
+        'img': 'https://raw.githubusercontent.com/binaryshrey/Profolyo/feat/onboarding/src/assets/integrations/linkedin.svg',
+        'description': 'LinkedIn is a business and employment-oriented service that operates via websites and mobile apps.',
+        'type': 'Social'
+    },
+    {
+        'name': 'Medium',
+        'url': 'https://medium.com/',
+        'img': 'https://raw.githubusercontent.com/binaryshrey/Profolyo/feat/onboarding/src/assets/integrations/medium.svg',
+        'description': 'Medium is an online publishing platform developed by Evan Williams, and launched in August 2012.',
+        'type': 'Blog'
+    },
+    {
+        'name': 'Pinterest',
+        'url': 'https://www.pinterest.com/',
+        'img': 'https://raw.githubusercontent.com/binaryshrey/Profolyo/feat/onboarding/src/assets/integrations/pinterest.svg',
+        'description': 'Pinterest is an American image sharing and social media service designed to enable saving and discovery of information on the internet using images and, on a smaller scale, animated GIFs and videos.',
+        'type': 'Design'
+    },
+    {
+        'name': 'Signal',
+        'url': 'https://signal.org/',
+        'img': 'https://raw.githubusercontent.com/binaryshrey/Profolyo/feat/onboarding/src/assets/integrations/signal.svg',
+        'description': 'Signal is a cross-platform encrypted messaging service developed by the Signal Foundation and Signal Messenger.',
+        'type': 'Communication'
+    },
+    {
+        'name': 'Skype',
+        'url': 'https://www.skype.com/',
+        'img': 'https://raw.githubusercontent.com/binaryshrey/Profolyo/feat/onboarding/src/assets/integrations/skype.svg',
+        'description': 'Skype is a telecommunications application that specializes in providing video chat and voice calls between computers, tablets, mobile devices, the Xbox One console, and smartwatches over the Internet.',
+        'type': 'Communication'
+    },
+    {
+        'name': 'Slack',
+        'url': 'https://slack.com/',
+        'img': 'https://raw.githubusercontent.com/binaryshrey/Profolyo/feat/onboarding/src/assets/integrations/slack.svg',
+        'description': 'Slack is a proprietary business communication platform developed by American software company Slack Technologies.',
+        'type': 'Communication'
+    },
+    {
+        'name': 'Telegram',
+        'url': 'https://telegram.org/',
+        'img': 'https://raw.githubusercontent.com/binaryshrey/Profolyo/feat/onboarding/src/assets/integrations/telegram.svg',
+        'description': 'Telegram is a freeware, cross-platform, cloud-based instant messaging software and application service.',
+        'type': 'Communication'
+    },
+    {
+        'name': 'Tik Tok',
+        'url': 'https://www.tiktok.com/',
+        'img': 'https://raw.githubusercontent.com/binaryshrey/Profolyo/feat/onboarding/src/assets/integrations/tiktok.svg',
+        'description': 'TikTok is a Chinese video-sharing social networking service owned by ByteDance.',
+        'type': 'Social'
+    },
+    {
+        'name': 'Twitch',
+        'url': 'https://www.twitch.tv/',
+        'img': 'https://raw.githubusercontent.com/binaryshrey/Profolyo/feat/onboarding/src/assets/integrations/twitch.svg',
+        'description': 'Twitch is a video live streaming service operated by Twitch Interactive, a subsidiary of Amazon.',
+        'type': 'Social'
+    },
+    {
+        'name': 'Twitter',
+        'url': 'https://www.x.xom/',
+        'img': 'https://raw.githubusercontent.com/binaryshrey/Profolyo/feat/onboarding/src/assets/integrations/twitter.svg',
+        'description': 'Twitter is an American microblogging and social networking service on which users post and interact with messages known as "tweets".',
+        'type': 'Social'
+    },
+    {
+        'name': 'Whatsapp',
+        'url': 'https://www.whatsapp.com/',
+        'img': 'https://raw.githubusercontent.com/binaryshrey/Profolyo/feat/onboarding/src/assets/integrations/whatsapp.svg',
+        'description': 'WhatsApp Messenger, or simply WhatsApp, is an American freeware, cross-platform centralized messaging and voice-over-IP service owned by Facebook, Inc.',
+        'type': 'Communication'
+    },
+    {
+        'name': 'Youtube',
+        'url': 'https://www.youtube.com/',
+        'img': 'https://raw.githubusercontent.com/binaryshrey/Profolyo/feat/onboarding/src/assets/integrations/youtube.svg',
+        'description': 'YouTube is an American online video-sharing platform headquartered in San Bruno, California.',
+        'type': 'Social'
+    },
+]
