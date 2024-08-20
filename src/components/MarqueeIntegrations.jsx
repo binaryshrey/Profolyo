a720d40a205e677d28f4c1febbbffcacba007c96
binaryshrey <shreyansh.saurabh2016@vitstudent.ac.in>
2024-08-20 15:59:15 +0530

    bug fixes

--- src/components/MarqueeIntegrations.jsx
+++ src/components/MarqueeIntegrations.jsx
@@ -7,22 +7,22 @@ export const MarqueeIntegrations = () => {
     <div className="relative flex h-[400px] w-full flex-col items-center justify-center overflow-hidden">
       <Marquee pauseOnHover className="[--duration:60s]">
         {integrations.map((item) => (
-          <img key={item.name} src={item.img} alt={item.name} className="w-16 h-16" />
+          <img key={item.name} src={item.img} alt={item.name} className="w-12 h-12" />
         ))}
       </Marquee>
       <Marquee reverse pauseOnHover className="[--duration:50s]">
         {integrations.map((item) => (
-          <img key={item.name} src={item.img} alt={item.name} className="w-16 h-16" />
+          <img key={item.name} src={item.img} alt={item.name} className="w-12 h-12" />
         ))}
       </Marquee>
       <Marquee pauseOnHover className="[--duration:40s]">
         {integrations.map((item) => (
-          <img key={item.name} src={item.img} alt={item.name} className="w-16 h-16" />
+          <img key={item.name} src={item.img} alt={item.name} className="w-12 h-12" />
         ))}
       </Marquee>
       <Marquee reverse pauseOnHover className="[--duration:30s]">
         {integrations.map((item) => (
-          <img key={item.name} src={item.img} alt={item.name} className="w-16 h-16" />
+          <img key={item.name} src={item.img} alt={item.name} className="w-12 h-12" />
         ))}
       </Marquee>
       <div className="pointer-events-none absolute inset-y-0 left-0 w-1/3 bg-gradient-to-r from-gray-100 dark:from-background"></div>
