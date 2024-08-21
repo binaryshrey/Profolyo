98d95f49b460c5e5698f73b63efd3924febbcedf
binaryshrey <shreyansh.saurabh2016@vitstudent.ac.in>
2024-08-21 16:35:05 +0530

    add tab switch in editor controller

--- src/views/editor/EditorController.jsx
+++ src/views/editor/EditorController.jsx
@@ -1,7 +1,24 @@
 import React from 'react';
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '../../components/tabs';
 
 const EditorController = ({ userData }) => {
-  return <div>EditorController</div>;
+  return (
+    <div className="mt-1">
+      <Tabs defaultValue="content">
+        <div className="items-center text-center">
+          <TabsList>
+            <TabsTrigger value="content">Content</TabsTrigger>
+            <TabsTrigger value="design">Design</TabsTrigger>
+          </TabsList>
+        </div>
+
+        <div className="pl-8">
+          <TabsContent value="content">Content</TabsContent>
+          <TabsContent value="design">Design</TabsContent>
+        </div>
+      </Tabs>
+    </div>
+  );
 };
 
 export default EditorController;
