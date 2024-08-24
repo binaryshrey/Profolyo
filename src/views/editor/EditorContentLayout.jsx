49ec5b64d77086f66b8abcd2f8c63b318c2343a1
binaryshrey <shreyansh.saurabh2016@vitstudent.ac.in>
2024-08-24 23:04:55 +0530

    add toogle size functionality

--- src/views/editor/EditorContentLayout.jsx
+++ src/views/editor/EditorContentLayout.jsx
@@ -2,59 +2,62 @@ import React, { useRef, useEffect } from 'react';
 import { Responsive, WidthProvider } from 'react-grid-layout';
 import 'react-grid-layout/css/styles.css';
 import 'react-resizable/css/styles.css';
-import { Button } from '../../components/button';
+import { v4 as uuidv4 } from 'uuid';
 import { ToggleGroup, ToggleGroupItem } from '../../components/toggle-group';
 
-// Wrap Responsive with WidthProvider
 const ResponsiveGridLayout = WidthProvider(Responsive);
 
 // Define layout for each breakpoint
 const layout = {
   xs: [
-    { i: 'a', x: 0, y: 0, w: 1, h: 2 },
-    { i: 'b', x: 0, y: 1, w: 1, h: 2 },
-    { i: 'c', x: 0, y: 2, w: 1, h: 2 },
-    { i: 'd', x: 0, y: 3, w: 1, h: 2 },
-    { i: 'e', x: 0, y: 4, w: 1, h: 2 },
+    { i: uuidv4(), x: 0, y: 0, w: 1, h: 2, size: 'small' },
+    { i: uuidv4(), x: 0, y: 0, w: 1, h: 2, size: 'medium' },
+    { i: uuidv4(), x: 0, y: 0, w: 1, h: 2, size: 'large' },
   ],
   sm: [
-    { i: 'a', x: 0, y: 0, w: 1, h: 2 },
-    { i: 'd', x: 2, y: 0, w: 1, h: 2 },
-    { i: 'b', x: 0, y: 0, w: 2, h: 2 },
-    { i: 'c', x: 0, y: 0, w: 2, h: 4 },
-    { i: 'e', x: 0, y: 0, w: 1, h: 4 },
+    { i: uuidv4(), x: 0, y: 0, w: 1, h: 2, size: 'small' },
+    { i: uuidv4(), x: 0, y: 0, w: 1, h: 2, size: 'medium' },
+    { i: uuidv4(), x: 0, y: 0, w: 2, h: 2, size: 'large' },
   ],
   md: [
-    { i: 'a', x: 0, y: 0, w: 1, h: 2 },
-    { i: 'b', x: 0, y: 0, w: 2, h: 2 },
-    { i: 'c', x: 0, y: 0, w: 2, h: 4 },
-    { i: 'd', x: 0, y: 0, w: 1, h: 2 },
-    { i: 'e', x: 0, y: 0, w: 1, h: 4 },
+    { i: uuidv4(), x: 0, y: 0, w: 1, h: 2, size: 'small' },
+    { i: uuidv4(), x: 0, y: 0, w: 2, h: 2, size: 'medium' },
+    { i: uuidv4(), x: 0, y: 0, w: 2, h: 4, size: 'large' },
   ],
-  lg: [
-    { i: 'a', x: 0, y: 0, w: 1, h: 2 },
-    { i: 'b', x: 1, y: 0, w: 2, h: 2 },
-    { i: 'c', x: 0, y: 0, w: 2, h: 4 },
-    { i: 'd', x: 0, y: 0, w: 1, h: 2 },
-    { i: 'e', x: 0, y: 0, w: 1, h: 4 },
-  ],
+};
+
+const layoutElementSize = {
+  xs: {
+    small: { w: 1, h: 2 },
+    medium: { w: 1, h: 2 },
+    large: { w: 1, h: 2 },
+  },
+  sm: {
+    small: { w: 1, h: 2 },
+    medium: { w: 1, h: 2 },
+    large: { w: 2, h: 2 },
+  },
+  md: {
+    small: { w: 1, h: 2 },
+    medium: { w: 2, h: 2 },
+    large: { w: 2, h: 4 },
+  },
 };
 
 const cols = {
   xs: 1,
   sm: 2,
   md: 4,
-  lg: 4,
 };
 
 const breakpoints = {
   xs: 480,
   sm: 767,
   md: 996,
-  lg: 1200,
 };
 
 const EditorContentLayout = () => {
+  const [editorLayout, setEditorLayout] = React.useState(layout);
   const [isDragging, setIsDragging] = React.useState(false);
   const [selectedWidget, setSelectedWidget] = React.useState(null);
 
@@ -62,29 +65,44 @@ const EditorContentLayout = () => {
     setSelectedWidget(widget);
   };
 
-  const handleDragStart = () => {
+  const handleDragStart = (layout, oldItem, newItem) => {
     setIsDragging(true);
   };
 
-  const handleDragStop = () => {
+  const handleDragStop = (layout, oldItem, newItem) => {
     setIsDragging(false);
   };
 
-  const widgetSizeToggle = ({ defValue }) => {
+  const updateLayout = (widget, size) => {
+    console.log('initial layout', editorLayout);
+    const updatedLayout = { ...editorLayout };
+
+    Object.keys(updatedLayout).forEach((breakpoint) => {
+      const sizeAttributes = layoutElementSize[breakpoint][size];
+      updatedLayout[breakpoint] = updatedLayout[breakpoint].map((item) => {
+        if (item.i === widget.i) {
+          return { ...item, w: sizeAttributes.w, h: sizeAttributes.h, size };
+        }
+        return item;
+      });
+    });
+
+    console.log('updated layout', updatedLayout);
+    setEditorLayout(updatedLayout);
+  };
+
+  const widgetSizeToggle = (widget) => {
     return (
-      <div className="bg-black w-12 h-48 rounded-full absolute items-center flex flex-col justify-center -ml-12">
-        <ToggleGroup type="single" className="flex flex-col" defaultValue={defValue}>
-          <ToggleGroupItem value="small" size="xs" variant="outlineDark" className="w-8">
+      <div className="bg-black w-10 h-40 rounded-full absolute items-center flex flex-col justify-center -ml-11 z-10">
+        <ToggleGroup type="single" className="flex flex-col" defaultValue={widget.size}>
+          <ToggleGroupItem value="small" size="xs" variant="outlineDark" className="w-8" onClick={() => updateLayout(widget, 'small')}>
             <p className="text-sm">S</p>
           </ToggleGroupItem>
-          <ToggleGroupItem value="medium" size="xs" variant="outlineDark" className="w-8">
+          <ToggleGroupItem value="medium" size="xs" variant="outlineDark" className="w-8" onClick={() => updateLayout(widget, 'medium')}>
             <p className="text-sm">M</p>
           </ToggleGroupItem>
-          <ToggleGroupItem value="large" size="xs" variant="outlineDark" className="w-8">
+          <ToggleGroupItem value="large" size="xs" variant="outlineDark" className="w-8" onClick={() => updateLayout(widget, 'large')}>
             <p className="text-sm">L</p>
-          </ToggleGroupItem>
-          <ToggleGroupItem value="extralarge" size="xs" variant="outlineDark" className="w-8">
-            <p className="text-sm">XL</p>
           </ToggleGroupItem>
         </ToggleGroup>
       </div>
@@ -93,25 +111,13 @@ const EditorContentLayout = () => {
 
   return (
     <div>
-      <ResponsiveGridLayout className="layout" breakpoints={breakpoints} layouts={layout} cols={cols} rowHeight={120} width={120} isResizable={false} isDraggable={true} onDragStart={handleDragStart} onDragStop={handleDragStop}>
-        <div key="a" className="relative">
-          {selectedWidget === 'a' && widgetSizeToggle('small')}
-          <div onClick={() => handleSelectedWidget('a')} className={`bg-blue-300 rounded-lg  flex items-center justify-center h-full ${isDragging ? 'cursor-grabbing' : 'cursor-pointer'} ${selectedWidget === 'a' ? 'border-4 border-black' : ''}`}>
-            Small Square
+      <ResponsiveGridLayout className="layout" breakpoints={breakpoints} layouts={editorLayout} cols={cols} rowHeight={120} width={120} isResizable={false} isDraggable={true} onDragStart={handleDragStart} onDragStop={handleDragStop}>
+        {editorLayout.md.map((item) => (
+          <div key={item.i} className="relative" onClick={() => handleSelectedWidget(item)}>
+            {selectedWidget?.i === item.i && widgetSizeToggle(item)}
+            <div className={`bg-blue-300 rounded-lg  flex items-center justify-center h-full ${isDragging ? 'cursor-grabbing' : 'cursor-pointer'} ${selectedWidget?.i === item.i ? 'border-4 border-black z-100' : ''}`}>{item.size}</div>
           </div>
-        </div>
-        <div key="b" onClick={() => handleSelectedWidget('b')} className={`bg-green-300 rounded-lg  flex items-center justify-center h-full ${isDragging ? 'cursor-grabbing' : 'cursor-pointer'} ${selectedWidget === 'b' ? 'border-4 border-black' : ''}`}>
-          Horizontal - Rectangle
-        </div>
-        <div key="c" onClick={() => handleSelectedWidget('c')} className={`bg-rose-300 rounded-lg  flex items-center justify-center h-full ${isDragging ? 'cursor-grabbing' : 'cursor-pointer'} ${selectedWidget === 'c' ? 'border-4 border-black' : ''}`}>
-          Large Square
-        </div>
-        <div key="d" onClick={() => handleSelectedWidget('d')} className={`bg-orange-300 rounded-lg  flex items-center justify-center h-full ${isDragging ? 'cursor-grabbing' : 'cursor-pointer'} ${selectedWidget === 'd' ? 'border-4 border-black' : ''}`}>
-          Small Square
-        </div>
-        <div key="e" onClick={() => handleSelectedWidget('e')} className={`bg-green-300 rounded-lg  flex items-center justify-center h-full ${isDragging ? 'cursor-grabbing' : 'cursor-pointer'} ${selectedWidget === 'e' ? 'border-4 border-black' : ''}`}>
-          Vertical - Rectangle
-        </div>
+        ))}
       </ResponsiveGridLayout>
     </div>
   );
