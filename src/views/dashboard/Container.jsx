b473cd5d5cc28db6ca01c231e37c0bd04ebfe5e8
binaryshrey <shreyansh.saurabh2016@vitstudent.ac.in>
2024-08-31 23:10:20 +0530

    add integrations

--- src/views/dashboard/Container.jsx
+++ src/views/dashboard/Container.jsx
@@ -116,8 +116,8 @@ const Container = ({ Component, board, integrations, settings }) => {
           </div>
           <main className="flex-1">
             <div className="py-6">
-              <div className="mx-auto max-w-7xl px-4 sm:px-6 md:px-8">
-                <div className="py-4 hidden lg:block">
+              <div className="mx-auto max-w-7xl">
+                <div className="py-16 hidden lg:block">
                   <Component />
                 </div>
                 <div className="py-2 overflow-y-auto lg:hidden">
--- src/views/dashboard/Integrations.jsx
+++ src/views/dashboard/Integrations.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import ConnectApps from '../onboard/ConnectApps';
 
 const Integrations = () => {
   return (
     <div>
-      <h1>Integrations</h1>
+      <div className="ml-8 mb-8">
+        <p className="text-2xl font-bold -mt-16 text-profolyo900">Integrations and Connected Apps</p>
+        <p className="text-profolyo900 text-zinc-600 mt-1">Supercharge your profolyo and connect the apps you use everyday.</p>
+      </div>
+
+      <ConnectApps />
     </div>
   );
 };
