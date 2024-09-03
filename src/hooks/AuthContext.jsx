d8514cf8098edd1d1c846760456998b320c6f503
binaryshrey <shreyansh.saurabh2016@vitstudent.ac.in>
2024-09-03 20:59:18 +0530

    check redirect

--- src/hooks/AuthContext.jsx
+++ src/hooks/AuthContext.jsx
@@ -15,7 +15,7 @@ export const AuthContextProvider = ({ children }) => {
       await supabase.auth.signInWithOAuth({
         provider: provider,
         options: {
-          redirectTo: 'https://profolyo.vercel.app/redirect/',
+          redirectTo: 'https://profolyo.vercel.app/auth/callback',
         },
       });
       console.log(`${provider} sign-in successful!`);
